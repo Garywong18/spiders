432f20d233d56e4e06509f4af35c8fafa72b43d9
Garywong18 <40932165+Garywong18@users.noreply.github.com>
2019-02-02 17:12:43 +0800

    Update pipelines.py

--- HR/pipelines.py
+++ HR/pipelines.py
@@ -4,8 +4,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from pymongo import MongoClient
+client = MongoClient() #实例化
+collection = client['tencent']['hr'] #设置数据库和集合
 
 class HrPipeline(object):
     def process_item(self, item, spider):
+        collection.insert(dict(item)) #将item转化为字典保存在mongdb
         return item
